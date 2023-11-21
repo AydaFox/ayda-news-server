fef70f667e69f04ed869b5fcba50811f420b582d
AydaFox <burrhe.y02@gmail.com>
2023-11-21 09:57:46 +0000

    refactors the advanced error handling to make queries more efficient and separate concerns in the models

--- models/comments.models.js
+++ models/comments.models.js
@@ -2,22 +2,11 @@ const db = require("../db/connection.js");
 
 exports.selectCommentsByArticleId = (article_id) => {
     return db.query(`
-            SELECT * 
-            FROM articles 
-            WHERE article_id = $1;`, [article_id])
+            SELECT *
+            FROM comments
+            WHERE article_id = $1
+            ORDER BY created_at DESC;`, [article_id])
         .then(({ rows }) => {
-            if (!rows.length) {
-                return Promise.reject({ status: 404, msg: "article not found" });
-            } else {
-                const commentQuery = db.query(`
-                        SELECT *
-                        FROM comments
-                        WHERE article_id = $1
-                        ORDER BY created_at DESC`, [article_id]);
-                return Promise.all([commentQuery]);
-            }
-        })
-        .then(([{ rows }]) => {
             return rows;
         });
 }
